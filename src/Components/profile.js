eb35cf0d5ddde82e978f69fb1d0f375e6e71045a
TemitopeOgunyemi <Ogunyemitemitope21@gmail.com>
2022-10-30 17:41:39 +0100

    slackname functionality

--- src/Components/profile.js
+++ src/Components/profile.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { useState } from "react";
 import style from "./profile.module.css";
 import ProfileImg from "../images/ProfileImg.jpg";
 
 const Profile = () => {
+ const [slackName, setSlackName]=  useState(false);
+
+ const showSlackHandler = ()=>{
+  setSlackName(true);
+ }
+
   return (
-    <div id={style.profileSection}>
+    <div id={style.profileSection} onClick={showSlackHandler}>
       <div id={style.imgContainer}>
         <img id={style["profile-img"]} src={ProfileImg} alt="Profile" />
       </div>
       <span id={style.twitter}>Temi_Ogunyemi</span>
-      <span id={style.slack}>Oluwakeshi</span>
+      {slackName && <span id={style.slack}>Oluwakeshi</span>}
     </div>
   );
 };
